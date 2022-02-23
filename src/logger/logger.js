fdca70ce2f49026e5ef650b902fec9d650f045b5
Arti-Khillare <agkhillare@gmail.com>
2022-02-23 22:25:49 +0530

    My First GitAssignment

---
+++ src/logger/logger.js
@@ -0,0 +1,10 @@
+let url = 'https://www.google.com'
+function printMyMessage(message){
+    console.log(message)
+}
+function welcome() {
+    console.log('Welcome to my application.I am Arti and aa part of FunctionUp thorium cohort.')
+}
+module.exports.url = this.url
+module.exports.printMessage = printMyMessage
+module.exports.printWelcomeMessage = welcome

ce1b69bb3cbd8b58f431f0d3ac3583aeaf4686ea
Arti-Khillare <agkhillare@gmail.com>
2022-02-26 02:45:44 +0530

    My PostAPI1 assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,79 @@
 const express = require('express');
 const router = express.Router();
+let players = [
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+        "swimming"
+        ]
+    },
+    {
+            "name": "manisha",
+            "dob": "1/1/1994",
+            "gender": "female",
+            "city": "jalana",
+            "sports": [
+            "badminton"
+            ]
+    },
+    {
+        "name": "manaswi",
+        "dob": "1/11/1999",
+        "gender": "female",
+        "city": "delhi",
+        "sports": [
+        "hocky"
+        ]
+        }
+        
+];
+
+
+router.post('/players', function(req, res) {
+   
+    let player = req.body;
+    let playerName = player.name
+    for(let i=0 ;i<players.length; i++){
+        if(players[i].name==playerName){
+            res.send('Player already exist')
+        }
+    }
+    players.push(player);
+    console.log('Here is the player from request',players);
+    res.send(players);
+});
+
+router.post('/players/:playerName/bookings/:bookingId', function(req, res){
+    let name = req.params.playerName
+    let isPlayerPresent = false
+
+    for(let i=0;i<players.length;i++){
+        if(players[i].name == name){
+            isPlayerPresent = true
+        }
+    }
+    if(!isPlayerPresent){
+        res.send('Player not present')
+    }
+    let booking = req.body
+    let bookingId = req.params.bookingId
+    for(let i=0;i<players.length;i++){
+        if(players[i].name == name){
+            //let isBookingPresent = false
+            for(let j=0;j<players[i].bookings.length;j++)
+            {
+                if(players[i].bookings[j].bookingNumber = bookingId){
+                    res.send('Booking with id is already present for tne player')
+                }
+            }
+            players[i].bookings.push(booking)
+        }
+    }
+    res.send(players)
+})
 
 router.get('/students/:name', function(req, res) {
     let studentName = req.params.name
